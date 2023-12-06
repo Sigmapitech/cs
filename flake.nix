6bbe9e31102d5a273e47834a990adfa0bf4dad1b
Sigmanificient <edhyjox@gmail.com>
2023-12-06 19:11:50 +0100

    Add back flake utils

--- flake.nix
+++ flake.nix
@@ -3,42 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
     ruleset = {
       url = "git+ssh://git@github.com/Epitech/banana-coding-style-checker.git";
       flake = false;
     };
   };
 
-  outputs = { nixpkgs, ruleset, ... }:
-    let
-      forAllSystems = function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]
-          (system:
-            let
-              pkgs = nixpkgs.legacyPackages.${system};
-            in
-            function { inherit system pkgs; });
+  outputs = { nixpkgs, ruleset, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     rec {
-      formatter = forAllSystems ({ pkgs, ... }: pkgs.nixpkgs-fmt);
+      formatter = pkgs.nixpkgs-fmt;
 
-      packages = forAllSystems (arch: rec {
-        report = import ./report.nix ruleset arch;
+      packages = rec {
+        report = import ./report.nix ruleset { inherit system pkgs; };
         default = report;
-      });
+      };
 
-      apps = forAllSystems ({ system, ... }: rec {
+      apps = rec {
         report = {
           type = "app";
-          program = "${packages.${system}.report}/bin/cs";
+          program = "${packages.report}/bin/cs";
         };
 
         default = report;
-      });
-    };
+      };
+    });
 }
