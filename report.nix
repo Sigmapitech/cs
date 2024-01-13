1831c39f462406f5485fbdb1cb8164bf4dd989cd
Sigmanificient <edhyjox@gmail.com>
2024-01-13 07:16:23 +0100

    Rewrite the script with python

--- report.nix
+++ report.nix
@@ -1,40 +1,6 @@
-pkgs: ruleset: banana-vera:
-(pkgs.writeShellScriptBin "cs" ''
-  start_time=$(date +%s)
-
-  if [ -z "$1" ]; then
-    project_dir=$(pwd)
-  else
-    project_dir="$1"
-  fi
-
-  echo "Running norm in $project_dir"
-
-  count=$(find "$project_dir"     \
-    -type f                       \
-    -not -path "*/.git/*"         \
-    -not -path "*/.idea/*"        \
-    -not -path "*/.vscode/*"      \
-    -not -path "*/bonus/*"        \
-    -not -path "*/tests/*"        \
-    -not -path "*/*build/*"       \
-  | ${banana-vera}/bin/vera++     \
-    --profile epitech             \
-    --root ${ruleset}/vera        \
-    --error                       \
-    2>&1                          \
-    | sed "s|$project_dir/||"     \
-    | tee /dev/stderr | wc -l
-  )
-
-  echo "Found $count issues"
-
-  end_time=$(date +%s)
-  echo "Ran in $((end_time - start_time))s"
-
-  if [ $count -gt 0 ]; then
-    exit 1
-  fi
-
-  exit 0
+pkgs: ruleset: banana-vera: let
+  report = (pkgs.writers.writePython3Bin "report" {}
+    (builtins.readFile ./report.py));
+in (pkgs.writeShellScriptBin "cs" ''
+${report}/bin/report ${banana-vera}/bin/vera++ ${ruleset}/vera $*
 '')
---
+++ report.py
@@ -0,0 +1,111 @@
+"""
+usage: report.py [banana-vera-bin] [ruleset] [path] [...]
+
+"""
+
+from typing import List, Optional
+
+import re
+import os
+import subprocess
+import shlex
+import sys
+import time
+
+
+IGNORE_PATTERNS: List[str] = [
+    r".*/[.]build/.*",
+    r".*/[.]cache/.*",
+    r".*/[.]direnv/.*",
+    r".*/[.]git/.*",
+    r".*/[.]idea/.*",
+    r".*/[.]vscode/.*",
+    r".*/bonus/.*",
+]
+
+
+def find_files(search_dir: str, ignore_tests: bool = False) -> List[str]:
+    find_proc = subprocess.run(
+        shlex.split(f"find {search_dir} -type f"),
+        capture_output=True, text=True
+    )
+
+    if ignore_tests:
+        IGNORE_PATTERNS.append(r".*/tests/.*")
+
+    return [
+        filename for filename in find_proc.stdout.splitlines()
+        if all(
+            re.match(pattern, filename) is None
+            for pattern in IGNORE_PATTERNS
+        )
+    ]
+
+
+def run_vera(
+    vera_bin: str, ruleset: str, basepath: str,
+    ignore_tests: bool = False, ignored_rules: Optional[List[str]] = None
+) -> int:
+    files = find_files(basepath, ignore_tests=ignore_tests)
+    vera_proc = subprocess.Popen(
+        shlex.split(f"{vera_bin} --profile epitech --root {ruleset}"),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        text=True
+    )
+
+    stdout, stderr = vera_proc.communicate(input='\n'.join(files))
+
+    if not stdout:
+        print(stderr)
+        return -1
+
+    reports = (
+        stdout if ignored_rules is None else
+        '\n'.join(
+            line for line in stdout.splitlines()
+            if not any(
+                re.match(rf".*:{rule}$", line)
+                for rule in ignored_rules
+            )
+        )
+    )
+
+    print(reports.replace(basepath.removesuffix('/'), '.'))
+    return sum(
+        reports.count(severity)
+        for severity in ("FATAL", "MAJOR", "MINOR", "INFO")
+    )
+
+
+def main() -> int:
+    if len(sys.argv) < 3:
+        return 1
+
+    vera, ruleset, *args = sys.argv[1::]
+    project_dir = os.path.expanduser(
+        args[0] if args and not args[0].startswith('-') else "."
+    )
+
+    ignored_rules = [
+        rule for arg in args for rule in arg[15:].split(",")
+        if arg.startswith("--ignore-rules=")
+    ]
+
+    marker = time.perf_counter()
+    count = run_vera(
+        vera, ruleset, project_dir,
+        ignore_tests="--ignore-tests" in args,
+        ignored_rules=ignored_rules
+    )
+
+    if count != -1:
+        elasped = time.perf_counter()
+        print(f"Found {count} issues")
+        print(f"Ran in {(elasped - marker):.3f}s")
+
+    return count > 0 and count != -1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
