37633373171643c98bdd96ec3c7d00afff412ff2
Ciznia <gabriel.hosquet@epitech.eu>
2024-03-28 22:37:49 +0100

    Add an flag to ignore some folders

--- report.py
+++ report.py
@@ -3,7 +3,7 @@ usage: report.py [banana-vera-bin] [ruleset] [path] [...]
 
 """
 
-from typing import List, Optional
+from typing import List
 
 import re
 import os
@@ -25,56 +25,60 @@ IGNORE_PATTERNS: List[str] = [
 ]
 
 
-def find_files(search_dir: str, ignore_tests: bool = False) -> List[str]:
+def find_files(search_dir: str, ignored_folders: List[str]) -> List[str]:
     find_proc = subprocess.run(
         shlex.split(f"find {search_dir} -type f"),
-        capture_output=True, text=True
+        capture_output=True,
+        text=True,
     )
 
-    if ignore_tests:
-        IGNORE_PATTERNS.append(r".*/tests/.*")
-
+    for folder in ignored_folders or []:
+        IGNORE_PATTERNS.append(rf".*/{folder}/.*")
     return [
-        filename for filename in find_proc.stdout.splitlines()
+        filename
+        for filename in find_proc.stdout.splitlines()
         if all(
-            re.match(pattern, filename) is None
-            for pattern in IGNORE_PATTERNS
+            re.match(pattern, filename) is None for pattern in IGNORE_PATTERNS
         )
     ]
 
 
 def run_vera(
-    vera_bin: str, ruleset: str, basepath: str,
-    ignore_tests: bool = False, ignored_rules: Optional[List[str]] = None
+    vera_bin: str,
+    ruleset: str,
+    basepath: str,
+    ignored_folders: List[str] = None,
+    ignored_rules: List[str] = None,
 ) -> int:
     print("Running norm in", basepath)
 
-    files = find_files(basepath, ignore_tests=ignore_tests)
+    files = find_files(basepath, ignored_folders)
     vera_proc = subprocess.Popen(
         shlex.split(f"{vera_bin} --profile epitech --root {ruleset}"),
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
-        text=True
+        text=True,
     )
 
-    stdout, stderr = vera_proc.communicate(input='\n'.join(files))
+    stdout, stderr = vera_proc.communicate(input="\n".join(files))
 
     if not stdout:
         print(stderr)
         return -1
 
     reports = (
-        stdout if ignored_rules is None else
-        '\n'.join(
-            line for line in stdout.splitlines()
+        stdout
+        if ignored_rules is None
+        else "\n".join(
+            line
+            for line in stdout.splitlines()
             if not any(
-                re.match(rf".*:{rule}$", line)
-                for rule in ignored_rules
+                re.match(rf".*:{rule}$", line) for rule in ignored_rules
             )
         )
     )
 
-    print(reports.replace(basepath.removesuffix('/'), '.'))
+    print(reports.replace(basepath.removesuffix("/"), "."))
     return sum(
         reports.count(severity)
         for severity in ("FATAL", "MAJOR", "MINOR", "INFO")
@@ -87,19 +91,32 @@ def main() -> int:
 
     vera, ruleset, *args = sys.argv[1::]
     project_dir = os.path.expanduser(
-        args[0] if args and not args[0].startswith('-') else "."
+        args[0] if args and not args[0].startswith("-") else "."
     )
 
     ignored_rules = [
-        rule for arg in args for rule in arg[15:].split(",")
+        rule
+        for arg in args
+        for rule in arg[15:].split(",")
         if arg.startswith("--ignore-rules=")
     ]
+    ignored_folders = [
+        folder
+        for arg in args
+        for folder in arg[17:].split(",")
+        if arg.startswith("--ignore-folders=")
+    ]
+
+    if "--include-tests" not in args:
+        ignored_folders.append("tests")
 
     marker = time.perf_counter()
     count = run_vera(
-        vera, ruleset, project_dir,
-        ignore_tests="--include-tests" not in args,
-        ignored_rules=ignored_rules
+        vera,
+        ruleset,
+        project_dir,
+        ignored_folders,
+        ignored_rules,
     )
 
     if count != -1:
