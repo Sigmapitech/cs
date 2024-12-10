52a81a36d1533dd8b50ab547d16c780e3b11d4df
Sigmanificient <edhyjox@gmail.com>
2024-12-10 21:46:41 +0100

    Declare CS_TRANSCIENT within clang parser

--- report.nix
+++ report.nix
@@ -1,6 +1,17 @@
 pkgs: ruleset: banana-vera: let
   report = (pkgs.writers.writePython3Bin "report" {}
     (builtins.readFile ./report.py));
+
+  patched-ruleset = pkgs.stdenv.mkDerivation {
+    name = "patched-ruleset";
+
+    src = ruleset;
+    dontBuild = true;
+    patches = [ ./declare_transcient_macro.patch ];
+
+    installPhase = "cp -r . $out";
+  };
+
 in (pkgs.writeShellScriptBin "cs" ''
-${report}/bin/report ${banana-vera}/bin/vera++ ${ruleset}/vera $*
+${report}/bin/report ${banana-vera}/bin/vera++ ${patched-ruleset}/vera $*
 '')
